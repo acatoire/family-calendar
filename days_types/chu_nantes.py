2d6b2b295872001ec653400b4b0c8200a1906e41
Axel CATOIRE <axel.catoire@gmail.com>
2024-01-07 18:49:37 +0100

    Add melyssa and only 3 colors

--- days_types/chu_nantes.py
+++ days_types/chu_nantes.py
@@ -8,13 +8,15 @@ from workdays.workdays import WorkDay
 
 # TODO #7 get them from dedicated sheet
 chu_days_types = {
-    "J": WorkDay(time(8, 30), time(16, 30), "J-Jour", color=7),
-    "M": WorkDay(time(6, 45), time(14, 15), "M-Matin", color=1),
-    "Mc": WorkDay(time(6, 45), time(14, 15), "Mc-Matin changeable", color=1),
+    "M": WorkDay(time(6, 45), time(14, 15), "M-Matin", color=5),
+    "Mc": WorkDay(time(6, 45), time(14, 15), "Mc-Matin changeable", color=5),
     "S": WorkDay(time(13, 45), time(21, 15), "S-Soir", color=5),
     "Sc": WorkDay(time(13, 45), time(21, 15), "Sc-Soir changeable", color=5),
-    "Jca": WorkDay(time(8, 30), time(16, 30), "Jca-Jour modifiable", color=8),
-    "Jrp": WorkDay(time(8, 30), time(16, 30), "Jrp-Jour modifiable", color=8),
+    "J": WorkDay(time(8, 30), time(16, 30), "J-Jour", color=5),
+    "Jca": WorkDay(time(8, 30), time(16, 30), "Jca-Jour modifiable", color=5),
+    "Jrp": WorkDay(time(8, 30), time(16, 30), "Jrp-Jour modifiable", color=5),
+    "N": WorkDay(time(21, 0), time(7, 0), "N-Nuit", color=11),
+    "Nca": WorkDay(time(21, 0), time(7, 0), "Nca-Nuit sur Ca de N", color=11),
     "TA": WorkDay(None, None, "TA-Repo rtt ?", is_off=True, color=10),
     "To": WorkDay(None, None, "To-Repo recup ?", is_off=True, color=10),
     "RF": WorkDay(None, None, "RF-Repo récupération férier", is_off=True, color=10),
@@ -26,6 +28,4 @@ chu_days_types = {
     "OFF": WorkDay(None, None, "OFF-Multi day off", is_off=True, color=10),
     "EM": WorkDay(None, None, "OFF-Enfant Malade", is_off=True, color=10),
     "AM": WorkDay(21, None, "OFF-Arret Maladie", is_off=True, color=10),
-    "N": WorkDay(time(21, 0), time(7, 0), "N-Nuit", color=11),
-    "Nca": WorkDay(time(21, 0), time(7, 0), "Nca-Nuit sur Ca de N", color=8),
 }
